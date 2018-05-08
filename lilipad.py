552621ffde1bb8d2bc0b357df8aba5dfd2b6d0a8
LarryGF <larrywtf609@gmail.com>
2018-05-08 16:49:35 -0400

    succesfully saves and load step2 tables

--- lilipad.py
+++ lilipad.py
@@ -1,5 +1,6 @@
 import eel
 from step_1 import step_1,step_1_load
+from step2 import step2_write,step2_load
 
 
 
@@ -14,11 +15,17 @@ def handler(step,table,dictio):
     print(dictio)
     if step == 1:
         step_1(dictio)
+    elif step ==2:
+        step2_write(table,dictio)
     
 @eel.expose
-def load(step,table,dictio):
+def load(step,table_list):
     if step == 1:
         values = step_1_load()
         return values
 
-eel.start('lilipad.html')
+    elif step == 2:
+        values = step2_load(table_list)
+        return values
+        
+eel.start('lili.html')
---
+++ web/step_3.py
@@ -0,0 +1,53 @@
+import json
+import zabixmaster as zb
+
+def step3_load(table_list):####diccionario con los nombres da las tablas
+    with open('step3.json') as f:
+        dic=json.load(f)
+    if 'IaaS' and 'DSaaS' in table_list:
+        for table in dic.keys():
+            lista=[]
+            for fila in dic[table].keys():  ####ordenar las filas al ingresarlas a la lista
+                lista.append(dic[table][fila])
+            dic[table]=lista
+        return dic        
+    else:
+        lista=[]
+        for fila in dic[table_list[0]].keys():  ####ordenar las filas al ingresarlas a la lista
+            lista.append(dic[table_list[0]][fila])
+        dic[table_list[0]]=lista       
+        return [table_list[0]]
+### nos va a dar un diccionario con la seleccion,
+### hacer un for para ver que tabla enviar 
+
+  
+def step3_write(table,dic):###nos devuelve nombre da tabla y lista con filas
+    with open('step3.json') as f:
+        d = json.load(f)
+    d[table] = dic
+    with open('step3.json','w') as f:
+        json.dump(d,f)
+
+def step5 (servicio,user,passw,db_addr,start_time,end_time):
+    dic3={}
+    dic= step2_load('tabla')######la funcion cambio,nesecitamos modificar estooooooooo
+    for row in dic.keys():
+        if dic[row]['servicio'] == servicio:
+            with open('parser.json') as f:
+                parse = json.load(f)
+            zabbix_id = parse[dic[row]['MV']]
+            now = start_time
+            delta = dt.td(1)
+            zb.db_connect(user,passw,db_addr)
+            while now!=end_time:
+                now +=delta
+                dic2=zb.hosts(zabbix_id,now,end_time) ##Numero de items id en zabbix de variables a monitorizar tabla 2.9          
+                dic3[now]=dic2    
+    
+    
+        
+parse={'SQUID01':'Squid01','SQUID02':'Squid02','SQUID03':'Squid03','SQUID04':'Squid04','SQUID05':'Squid05','SQUID06-RECTORA':'Squid06','SQUID07':'Squid07'
+        'SQUID08-228':'Squid08','SQUID09':'Squid09','SQUID10':'Squid10','SQUID11-229':'Squid11','SQUID12-230':'Squid12','SQUID13':'Squid13','SQUID14':'Squid14',
+        }    
+    
+        
